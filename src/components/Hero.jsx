0c386d8fdf5f9e9aef890ea08cb3290cb2dd891f
aaronmullan <aaroncmullan@gmail.com>
2022-10-17 14:13:26 -0700

    further heroics

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import heroStyles from './heroStyles';
+
+export default function Hero() {
+  return (
+    <div style={heroStyles}>
+      <div><h1>Aaron Mullan: Developer</h1></div>
+
+    </div>
+  );
+}
---
+++ src/components/heroStyles.js
@@ -0,0 +1,10 @@
+const heroStyles = {
+  display: 'flex',
+  justifyContent: 'center',
+  color: '#66d9ef',
+  border: '1px solid #7d7c78',
+  width: '80vw',
+  borderRadius: '.15em',
+};
+
+export default heroStyles;
