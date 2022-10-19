901a462932fa46ad1fd5398a7292c51d248ffa8a
aaronmullan <aaroncmullan@gmail.com>
2022-10-19 15:11:10 -0700

    add data and assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hero from './components/Hero';
 import Header from './components/Header';
+import CardGrid from './components/CardGrid';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <Hero />
+      <CardGrid />
     </div>
   );
 }
---
+++ src/components/CardGrid.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './cardGrid.css';
+
+export default function CardGrid() {
+  return (
+    <div id="grid">
+      <p>hello</p>
+    </div>
+  );
+}
