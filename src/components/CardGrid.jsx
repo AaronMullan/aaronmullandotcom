4a41ce627c60debafb35b0769e5d8d684d6c7037
aaronmullan <aaroncmullan@gmail.com>
2022-10-20 09:03:21 -0700

    cardGrid MVP

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import cardData from '../data/cardData';
 import './cardGrid.css';
 
 export default function CardGrid() {
   return (
     <div id="grid">
-      <p>hello</p>
+      {cardData.map(({
+        title, text, image, link, linkText,
+      }) => (
+        <div className="card">
+          <img src={image} alt="" />
+          <div className="card-body">
+            <h3>{title}</h3>
+            <p>{text}</p>
+          </div>
+          <div className="card-footer">
+            <a href={link}>
+              <button type="button" className="button primary-button">
+                {linkText || 'site'}
+              </button>
+            </a>
+          </div>
+        </div>
+      ))}
+      );
     </div>
   );
 }
